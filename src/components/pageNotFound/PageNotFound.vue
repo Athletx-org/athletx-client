ee59a4c6c1f6d1cf4b9a3235617c7b08e2bdd47a
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-08-22 19:12:30 +0200

    chore: create some sample components

---
+++ src/components/pageNotFound/PageNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Page Not Found!</h1>
+  <img src="../../assets/404.jpg" />
+</template>
+  
+<script>
+export default {
+  name: "PageNotFound",
+  data: () => ({}),
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
