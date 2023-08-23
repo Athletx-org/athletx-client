c30970bef3794770d320eefaab302fab93ea372f
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-08-23 15:27:20 +0200

    chore: try request to server

--- src/components/homepage/HomePage.vue
+++ src/components/homepage/HomePage.vue
@@ -1,11 +1,35 @@
 <template>
     <div class="home">
         <h1> HomePage</h1>
+        <ul>
+            <li v-for="number in serverMsg" :key="number">{{ number }}</li>
+          </ul>
     </div>
 </template>
-  
 <script>
+
+import api from "../../services/api"
+
+
 export default {
     name: "HomePage",
+    data() {
+        return {
+            serverMsg: []
+        }
+    },
+    created() {
+        this.tryServerRequest();
+    },
+    methods: {
+        async tryServerRequest() {
+            try {
+                const response = await api.provaGet()
+                this.serverMsg = response.data()
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }
 };
 </script>
