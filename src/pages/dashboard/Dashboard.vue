e94eae99342087dc95dd817589ca871a68f8ed82
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-08-31 22:33:21 +0200

    chore: add routes for dashboard components

--- src/pages/dashboard/Dashboard.vue
+++ src/pages/dashboard/Dashboard.vue
@@ -1,34 +1,12 @@
 <template>
-    <div>
-        {{ data }}
-    </div>
-  </template>
-  
-  <script>
-import userService from '@/services/user.service';
-  
-  export default {
-    data() {
-      return {
-        data: ""
-      };
-    },
-    created() {
-      this.fetchData();
-    },
-    methods: {
-      fetchData() {
-        userService.getUserInfo()
-          .then((response) => {
-            console.log(response)
-            this.data = "profile";
-          })
-          .catch(error => {
-            console.error('Errore durante la richiesta:', error);
-          });
-      },
-    }
-  };
-  </script>
+    <h1>Dashboard</h1>
+</template>
 
-<style src="./Dashboard.scss" lang="scss" scoped />
+<script>
+export default {
+  name: "Dashboard",
+  data: () => ({}),
+}
+</script>
+
+<style src="./Dashboard.scss" lang="scss" scoped/>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,43 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 
-import HomePage from "../pages/home/HomePage.vue";
-import PageNotFound from "../pages/notFound/PageNotFound.vue";
-import Login from "../pages/login/Login.vue";
-import Signup from "../pages/signup/Signup.vue";
-import Dashboard from "../pages/dashboard/Dashboard.vue";
+import HomePage from "@/pages/home/HomePage.vue";
+import PageNotFound from "@/pages/notFound/PageNotFound.vue";
+import Login from "@/pages/login/Login.vue";
+import Layout from "@/pages/layout/Layout";
+import Dashboard from "@/pages/dashboard/Dashboard";
+import Signup from "@/pages/signup/Signup.vue";
 import store from '@/store'
-
+import Profile from "@/pages/profile/Profile";
 
 const routes = [
-  { path: "/", component: HomePage, alias: "/home" },
+  {
+    path: "/",
+    component: HomePage,
+    alias: "/home"
+  },
+
   { path: "/login", name: "login", component: Login },
   { path: "/signup", name: "signup", component: Signup },
-  { path: "/dashboard", name: "dashboard", component: Dashboard, meta:{requiresAuth:true}},
+  {
+    path: "/dashboard",
+    component: Layout,
+    meta:{
+      requiresAuth:true
+    },
+    children: [
+    {
+      path: '',
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    ]
+  },
+
   { path: "/:pathMatch(.*)*", name: "not-found", component: PageNotFound },
   { path: "/:pathMatch(.*)", name: "bad-not-found", component: PageNotFound },
 ];
@@ -22,10 +47,16 @@ const router = createRouter({
   routes,
 });
 
+function isLoggedIn() {
+  return store.state.auth.status.loggedIn
+}
+
 router.beforeEach((to, from, next) => {
+  if ( (to.path === '/' || to.path==='/home' ) && isLoggedIn()) {
+    next('/dashboard')
+  }
   if (to.meta.requiresAuth) {
-    const auth = store.state.auth.status.loggedIn
-    if (auth) {
+    if (isLoggedIn()) {
       next();
     } else {
       next('/login');
