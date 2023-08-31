41ee01e75a22a5b762c275875604e1ba740bbc8c
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-08-31 22:32:58 +0200

    chore: create profile component

---
+++ src/pages/profile/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Profile</h1>
+</template>
+
+<script>
+
+export default {
+  name: "Profile",
+  data: () => ({}),
+}
+</script>
+
+<style src="./Profile.scss" lang="scss" scoped/>
