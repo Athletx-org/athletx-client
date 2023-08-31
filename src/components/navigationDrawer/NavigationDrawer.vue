f336cdeff5320e7cd1e1743fe877adcbb7868acd
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-08-31 22:32:48 +0200

    chore: create navigation drawer component

---
+++ src/components/navigationDrawer/NavigationDrawer.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card>
+        <v-navigation-drawer
+            app
+            v-model="isDrawerVisible"
+        >
+          <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" value="dashboard" to="/dashboard"></v-list-item>
+            <v-list-item prepend-icon="mdi-forum" title="Workouts" value="workouts" to="/"></v-list-item>
+            <v-list-item prepend-icon="mdi-forum" title="Profile" value="profile" to="/profile"></v-list-item>
+            <v-list-item prepend-icon="mdi-forum" title="Analytics" value="analytics" to="/"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+    </v-card>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  name: "NavigationDrawer",
+  computed: {
+    ...mapGetters('drawer', ['isDrawerVisible'])
+  },
+}
+</script>
+<style src="./NavigationDrawer.scss" lang="scss" scoped />
