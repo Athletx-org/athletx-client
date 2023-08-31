cfca6a92378003502d5d56004482ba6443371a6b
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-08-31 22:32:21 +0200

    chore: create dashboard app bar

---
+++ src/components/dashboardAppBar/DashboardAppBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar app color="light">
+    <v-btn
+        elevation="4"
+        @click="toggleDrawerVisibility"
+    >
+      <v-icon icon="$vuetify"/>
+      <v-spacer></v-spacer>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn color="black" @click.prevent="logout" class="mr-5">
+      <span>Logout</span>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import {mapActions} from "vuex";
+
+export default {
+  name: "DashboardAppBar",
+  methods: {
+    ...mapActions('drawer', ['toggleDrawerVisibility']),
+    logout() {
+      this.$store.dispatch("auth/logout")
+      this.$router.push("/home")
+    }
+  }
+}
+</script>
+
+<style src="./DashboardAppBar.scss" lang="scss" scoped/>
