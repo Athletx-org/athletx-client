6c84f6d35682523c0d21f22d248f13806e6b452a
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-09-05 16:09:05 +0200

    feat: create workout component

--- src/pages/workouts/Workouts.vue
+++ src/pages/workouts/Workouts.vue
@@ -1,5 +1,28 @@
 <template>
-<h1> {{ title }}</h1>
+  <h1> {{ title }}</h1>
+  <v-btn to="/newWorkout" color="success">Add new workout</v-btn>
+    <v-row>
+      <v-col class="mt-5" cols="12" sm="3" md="4" v-for="workout in this.workouts" :key="workout._id">
+        <v-card width="350" elevation="5" outlined>
+          <v-card-title> {{ workout.name }}</v-card-title>
+          <v-card-text>
+            {{ workout.description }}
+          </v-card-text>
+          <v-card-actions>
+            <v-btn size="small" color="secondary" variant="elevated" @click="setAsCurrent(workoutId)">
+              SET AS CURRENT
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn @click="editWorkout(workout._id)">
+              <v-icon size="small">mdi-pencil</v-icon>
+            </v-btn>
+            <v-btn @click="deleteWorkout(workout._id)">
+              <v-icon size="small">mdi-delete</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
 </template>
 <script>
 import WorkoutService from "@/services/workout.service";
@@ -9,17 +32,34 @@ export default {
   data() {
     return {
       title: "Workouts",
+      userId: this.$store.state.auth.user._id,
       workouts: [],
+      currentWorkout: null
     }
   },
-  created() {
-    this.workouts = this.fetchWorkouts()
+  mounted() {
+    this.fetchWorkouts()
+    //this.fetchCurrentWorkout()
   },
   methods: {
     async fetchWorkouts() {
-      return await WorkoutService.getWorkouts(this.$store.state.auth.user._id)
+      this.workouts = await WorkoutService.getWorkouts(this.userId)
+    },
+    async fetchCurrentWorkout() {
+      this.currentWorkout = await WorkoutService.getCurrentWorkout(this.userId)
+    },
+    async deleteWorkout(workoutId) {
+      WorkoutService.deleteWorkout(this.userId, workoutId).then(() => {
+        this.workouts = this.workouts.filter(workout => workout._id !== workoutId)
+      })
+    },
+    editWorkout(workoutId){
+      console.log(workoutId)
+    },
+    setAsCurrent(workoutId) {
+      console.log(workoutId)
     }
   }
 }
 </script>
-<style src="./Workouts.scss" lang="scss" scoped />
+<style src="./Workouts.scss" lang="scss" scoped/>
