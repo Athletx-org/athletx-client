05d19f4585147b244bd7cec324c091ab785085f7
giacomo accursi <giacomo.accursi@gmail.com>
2023-09-05 16:36:05 +0200

    feat: create new workout component

--- src/pages/newWorkout/NewWorkout.vue
+++ src/pages/newWorkout/NewWorkout.vue
@@ -3,10 +3,14 @@
     <v-row justify="center">
       <v-col cols="12" xs="12" md="6" l="5" xl="4">
         <div>
+          <v-text-field v-model="workout.name" label="Name"></v-text-field>
+          <v-text-field v-model="workout.description" label="Description"></v-text-field>
+          <v-text-field v-model="workout.difficulty" label="Difficulty"></v-text-field>
+          <v-text-field v-model="workout.duration" label="Duration"></v-text-field>
           <v-btn @click="addTraining">New Training</v-btn>
-          <draggable v-model="trainings" @start="onDragStart" @end="onDragEnd">
-            <div v-for="(training, tIndex) in trainings" :key="tIndex">
-              <v-card>
+          <draggable v-model="workout.trainings" @start="onDragStart" @end="onDragEnd">
+            <div v-for="(training, tIndex) in workout.trainings" :key="tIndex">
+              <v-card class="mb-3">
                 <v-card-title>
                   Training {{ tIndex + 1 }}
                   <v-btn @click="removeTraining(tIndex)">Delete</v-btn>
@@ -15,14 +19,15 @@
                   <v-btn @click="addExercise(training)">Add Exercise</v-btn>
                   <draggable v-model="training.exercises" @start="onDragStart" @end="onDragEnd">
                     <div v-for="(exercise, exIndex) in training.exercises" :key="exIndex">
-                      <v-card>
+                      <v-card class="mt-2">
                         <v-card-title>
                           Exercise {{ exIndex + 1 }}
                           <v-btn @click="removeExercise(training, exIndex)">Delete</v-btn>
                         </v-card-title>
                         <v-card-text>
                           <div>
-                            <v-btn v-show="!training.exercises[exIndex].exerciseId" @click="openDialog(exIndex)">Choose
+                            <v-btn class="mt-3 mb-3" v-show="!training.exercises[exIndex].exerciseId"
+                              @click="openDialog(exIndex)">Choose
                               Exercise </v-btn>
                             <v-dialog v-model="dialog" max-width="500px">
                               <v-card>
@@ -32,11 +37,10 @@
                                   <v-text-field autofocus v-model="searchQuery" label="Search Exercise" />
 
                                   <!-- Contenuto della "pop-up page" -->
-                                  <v-list> 
-                                    <v-list-item v-for="(default_exercise) in filterExercises()" :key="default_exercise">
-                                      <v-list-item
-                                        @click="() => selectExercise(training, default_exercise, exIndex)">
-                                        {{ default_exercise }}
+                                  <v-list>
+                                    <v-list-item v-for="(ex, Index) in filterExercises()" :key="Index">
+                                      <v-list-item @click="() => selectExercise(training, ex._id, exIndex)">
+                                        {{ ex.name }} (Muscle: {{ ex.muscle }}, Type: {{ ex.type }})
                                       </v-list-item>
                                     </v-list-item>
                                   </v-list>
@@ -47,7 +51,8 @@
                           </div>
                           <!-- Elemento selezionato -->
                           <div v-if="training.exercises[exIndex].exerciseId">
-                            <v-chip>{{ training.exercises[exIndex].exerciseId }}</v-chip>
+                            <!-- <v-chip>{{ this.default_exercises[training.exercises[exIndex].exerciseId] }}</v-chip> -->
+                            <v-chip>{{ this.default_exercises.find(item => item._id === training.exercises[exIndex].exerciseId).name }}</v-chip>
                             <v-btn @click="deleteSelectedExercise(training, exIndex)">x</v-btn>
                           </div>
                           <v-row>
@@ -61,7 +66,8 @@
                               <v-text-field v-model="training.exercises[exIndex].rest" label="Rest (sec)"></v-text-field>
                             </v-col>
                             <v-col cols="6" m="3" l="3" xl="3">
-                              <v-text-field v-model="training.exercises[exIndex].duration" label="Duration (min)"></v-text-field>
+                              <v-text-field v-model="training.exercises[exIndex].duration"
+                                label="Duration (min)"></v-text-field>
                             </v-col>
                           </v-row>
                         </v-card-text>
@@ -73,6 +79,7 @@
               </v-card>
             </div>
           </draggable>
+          <v-btn class="mt-5" @click="sendWorkout()">Send Workout</v-btn>
         </div>
       </v-col>
     </v-row>
@@ -82,6 +89,7 @@
 <script>
 import { ref } from 'vue';
 import { VueDraggableNext } from 'vue-draggable-next'
+import WorkoutService from "@/services/workout.service";
 
 export default {
   components: {
@@ -90,17 +98,32 @@ export default {
 
   data() {
     return {
-      trainings: [],
+      workout: {
+        name: "giacomo",
+        difficulty: "",
+        description: "",
+        duration: "",
+        trainings: []
+      },
       isDragging: false,
       dialog: false,
       dialogIndex: "",
-      default_exercises: ["Elemento 1", "Elemento 2", "Elemento 3"],
-      searchQuery: ref(""), 
+      // default_exercises: {
+      //   "64f1d9490d80d7192be5e549": "pushup",
+      //   "64f1d98a0d80d7192be5e54a": "pulley",
+      //   "64f1d9ba0d80d7192be5e54c": "running",
+      //   "64f1d9f30d80d7192be5e54d": "crunch"
+      // },
+      default_exercises: "", 
+      searchQuery: ref(""),
     };
+  },
+  mounted() {
+    this.getDefaultExercise()
   },
   methods: {
     addTraining() {
-      this.trainings.push({
+      this.workout.trainings.push({
         exercises: [],
       });
     },
@@ -111,11 +134,10 @@ export default {
         series: "",
         rest: "",
         duration: ""
-        // Aggiungi altre informazioni sull'esercizio qui
       });
     },
     removeTraining(tIndex) {
-      this.trainings.splice(tIndex, 1);
+      this.workout.trainings.splice(tIndex, 1);
     },
     removeExercise(training, exIndex) {
       training.exercises.splice(exIndex, 1);
@@ -128,24 +150,35 @@ export default {
     },
     openDialog(index) {
       this.dialog = true;
-      this.dialogIndex=index
+      this.dialogIndex = index
     },
     closeDialog() {
       this.dialog = false;
+      this.searchQuery = ""
     },
     selectExercise(training, exercise) {
       training.exercises[this.dialogIndex].exerciseId = exercise
-      this.searchQuery = ""
       this.closeDialog();
-      
+
     },
     deleteSelectedExercise(training, exIndex) {
       training.exercises[exIndex].exerciseId = null;
     },
-    filterExercises(){
-        return this.default_exercises.filter((default_exercise) => default_exercise.toLowerCase().includes(this.searchQuery.toLowerCase()))
+    filterExercises() {
+      // return Object.fromEntries(
+      //   Object.entries(this.default_exercises).filter(([value]) =>
+      //     value.toLowerCase().includes(this.searchQuery.toLowerCase())
+      //   )
+      // );
+      return this.default_exercises; 
+    },
+    sendWorkout() {
+      WorkoutService.createWorkout(this.$store.state.auth.user._id, this.workout)
+    },
+    async getDefaultExercise() {
+      this.default_exercises = await WorkoutService.getDefaultExercise()
+      console.log(this.default_exercises)
     }
   },
-
 };
 </script>
