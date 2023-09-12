b3e9cf4e1ac19f97f0131f971d51ea459798d46a
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-09-12 16:11:43 +0200

    fix: fix profile methods

--- src/pages/profile/Profile.vue
+++ src/pages/profile/Profile.vue
@@ -1,15 +1,18 @@
 <!-- ProfileCard.vue -->
 <template>
   <v-card>
-    <v-card-title class="headline">
-      {{ editing ? 'Edit Profile' : 'Profile Information' }}
-    </v-card-title>
     <v-card-text>
       <v-row>
         <v-col cols="12" sm="6">
           <v-avatar size="150">
-            <img src="https://randomuser.me/api/portraits/men/78.jpg" alt="Profile" />
+            <img :src="profilePicPath" alt="Profile" />
           </v-avatar>
+          <v-file-input
+              v-if="editing"
+              label="Profile Picture"
+              accept="image/*"
+              @change="handlePictureChange"
+          ></v-file-input>
         </v-col>
         <v-col cols="12" sm="6">
           <div v-if="editing">
@@ -17,6 +20,12 @@
             <v-text-field v-model="user.surname" label="Surname"></v-text-field>
             <v-text-field v-model="user.weight" label="Weight"></v-text-field>
             <v-text-field v-model="user.height" label="Height"></v-text-field>
+            <div>
+              <v-btn-toggle v-model="user.gender" rounded class="rounded-pill mb-4">
+                <v-btn :value="male" rounded class="flat" color="blue">Male</v-btn>
+                <v-btn :value="female" class="flat" :color="'pink'">Female</v-btn>
+              </v-btn-toggle>
+            </div>
           </div>
           <div v-else>
             <!-- Display-only fields -->
@@ -31,20 +40,28 @@
     <v-card-actions>
       <v-btn v-if="!editing" @click="toggleEdit"> Edit Profile </v-btn>
       <v-btn v-if="editing" @click="updateProfile"> Save </v-btn>
-
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-
 import UserService from "@/services/user.service";
+
 export default {
   data() {
     return {
       userId: this.$store.state.auth.user._id,
       editing: false,
-      user: {}
+      user: {
+        name: '',
+        surname: '',
+        weight: '',
+        height: '',
+        gender: '',
+        profilePic: '',
+      },
+      inputProfilePic: '',
+      profilePicPath: ''
     };
   },
   created() {
@@ -55,12 +72,23 @@ export default {
       this.editing = !this.editing;
     },
     async fetchUserInfo() {
-      this.user = await UserService.getUserInfo(this.userId)
-      console.log(this.user.name)
+      await UserService.getUserInfo(this.userId).then(
+          res => {
+            if (res != null) {
+              this.user = res
+              this.profilePicPath = process.env.VUE_APP_BASE_URL + this.user.profilePic
+            }
+          }
+      )
     },
-    updateProfile() {
-      console.log(this.user)
+    async updateProfile() {
       this.toggleEdit()
+      this.user.profilePic = this.inputProfilePic
+      await UserService.updateUserInfo(this.userId, this.user)
+      await this.fetchUserInfo()
+    },
+    handlePictureChange(event) {
+      this.inputProfilePic = event.target.files[0];
     }
   },
 };
