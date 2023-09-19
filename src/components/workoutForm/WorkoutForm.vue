7f5550f4d58f9add0c06e6e5e603c3413afa6b20
giacomo accursi <giacomo.accursi@gmail.com>
2023-09-19 18:51:57 +0200

    style: improve workout form style

--- src/components/workoutForm/WorkoutForm.vue
+++ src/components/workoutForm/WorkoutForm.vue
@@ -1,33 +1,56 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col cols="12" xs="12" md="6" l="5" xl="4">
+      <v-col cols="12" xs="12" md="8" l="6" xl="6">
         <div>
-          <v-text-field v-model="workout.name" label="Name"></v-text-field>
-          <v-text-field v-model="workout.description" label="Description"></v-text-field>
-          <v-text-field v-model="workout.difficulty" label="Difficulty"></v-text-field>
-          <v-text-field v-model="workout.duration" label="Duration"></v-text-field>
-          <v-btn @click="addTraining">New Training</v-btn>
+          <v-text-field v-model="workout.name" label="Name" variant="outlined"></v-text-field>
+          <v-text-field v-model="workout.description" label="Description" variant="outlined"></v-text-field>
+          <div class="mb-5">
+            <div class="font-weight-bold mb-2">Difficulty</div>
+            <v-btn v-for="(button, index) in difficultyButtons" variant="outlined" :ripple="false" :key="index" :class="{
+              'active-green': workout.difficulty === index && button === 'easy',
+              'active-yellow': workout.difficulty === index && button === 'medium',
+              'active-red': workout.difficulty === index && button === 'hard',
+            }" @click="setDifficulty(index)" class="mr-3">
+              {{ button }}
+            </v-btn>
+          </div>
+          <div class="mb-7">
+            <div class="font-weight-bold">Duration (min)</div>
+            <VueScrollPicker :options="durationArray" v-model="workout.duration" />
+          </div>
+          <v-btn @click="addTraining" variant="outlined" class="mb-4">New Training</v-btn>
           <draggable v-model="workout.trainings" @start="onDragStart" @end="onDragEnd">
             <div v-for="(training, tIndex) in workout.trainings" :key="tIndex">
-              <v-card class="mb-3">
+              <v-card variant="outlined" class="mb-3">
                 <v-card-title>
                   Training {{ tIndex + 1 }}
-                  <v-btn @click="removeTraining(tIndex)">Delete</v-btn>
+                  <v-btn class="ml-2" density="compact" variant="flat" size="medium" @click="removeTraining(tIndex)">
+                    <v-icon color="red">mdi-delete</v-icon>
+                  </v-btn>
                 </v-card-title>
                 <v-card-text>
-                  <v-btn @click="addExercise(training)">Add Exercise</v-btn>
+                  <v-btn variant="outlined" class="mb-2" @click="addExercise(training)">Add Exercise</v-btn>
                   <draggable v-model="training.exercises" @start="onDragStart" @end="onDragEnd">
                     <div v-for="(exercise, exIndex) in training.exercises" :key="exIndex">
-                      <v-card class="mt-2">
-                        <v-card-title>
+                      <v-card variant="outlined" class="mt-2">
+                        <v-card-title class="mb-4">
                           Exercise {{ exIndex + 1 }}
-                          <v-btn @click="removeExercise(training, exIndex)">Delete</v-btn>
+                          <v-btn class="ml-2" density="compact" variant="flat" size="medium"
+                            @click="removeExercise(training, exIndex)">
+                            <v-icon color="red">mdi-delete</v-icon>
+                          </v-btn>
                         </v-card-title>
                         <v-card-text>
-                          <div>
-                            <v-btn class="mt-3 mb-3" v-show="!training.exercises[exIndex].exerciseId"
-                              @click="openDialog(exIndex)">Choose
+                          <div v-if="training.exercises[exIndex].exerciseId" class="mb-8">
+                            <v-chip size="large" label v-if="default_exercises" closable
+                              @click:close="deleteSelectedExercise(training, exIndex)" text-color="white"
+                              close-icon="mdi-delete">
+                              {{ getDefaultExerciseName(training, exIndex) }}
+                            </v-chip>
+                          </div>
+                          <div v-else class="mb-8">
+                            <v-btn variant="outlined" @click="openDialog(exIndex)">Choose
                               Exercise </v-btn>
                             <v-dialog v-model="dialog" max-width="500px">
                               <v-card>
@@ -47,27 +70,25 @@
                               </v-card>
                             </v-dialog>
                           </div>
-                          <div v-if="training.exercises[exIndex].exerciseId">
-                           <v-chip v-if="default_exercises">{{ getDefaultExerciseName(training, exIndex) }}</v-chip>
-                            <v-btn @click="deleteSelectedExercise(training, exIndex)">x</v-btn>
-                          </div>
                           <v-row>
                             <v-col cols="6" m="3" l="3" xl="3">
-                              <v-text-field v-model="training.exercises[exIndex].reps" label="Reps"></v-text-field>
+                              <v-text-field v-model="training.exercises[exIndex].reps" variant="outlined"
+                                label="Reps"></v-text-field>
                             </v-col>
                             <v-col cols="6" m="3" l="3" xl="3">
-                              <v-text-field v-model="training.exercises[exIndex].series" label="Series"></v-text-field>
+                              <v-text-field v-model="training.exercises[exIndex].series" variant="outlined"
+                                label="Series"></v-text-field>
                             </v-col>
                             <v-col cols="6" m="3" l="3" xl="3">
-                              <v-text-field v-model="training.exercises[exIndex].rest" label="Rest (sec)"></v-text-field>
+                              <v-text-field v-model="training.exercises[exIndex].rest" variant="outlined"
+                                label="Rest (sec)"></v-text-field>
                             </v-col>
                             <v-col cols="6" m="3" l="3" xl="3">
-                              <v-text-field v-model="training.exercises[exIndex].duration"
+                              <v-text-field v-model="training.exercises[exIndex].duration" variant="outlined"
                                 label="Duration (min)"></v-text-field>
                             </v-col>
                           </v-row>
                         </v-card-text>
-
                       </v-card>
                     </div>
                   </draggable>
@@ -75,7 +96,10 @@
               </v-card>
             </div>
           </draggable>
-          <v-btn class="mt-5" @click="sendWorkout()">Send Workout</v-btn>
+          <div class="text-center">
+            <v-btn color="paletteBlue" size="large" variant="flat" class="mt-5" @click="sendWorkout()">Send
+              Workout</v-btn>
+          </div>
         </div>
       </v-col>
     </v-row>
@@ -86,25 +110,31 @@
 import { ref } from 'vue';
 import { VueDraggableNext } from 'vue-draggable-next'
 import WorkoutService from "@/services/workout.service";
+import { VueScrollPicker } from 'vue-scroll-picker'
 
 export default {
   components: {
     draggable: VueDraggableNext,
+    VueScrollPicker: VueScrollPicker
   },
   props: {
     workoutData: Object
   },
+
   data() {
     return {
+      durationArray: Array.from({ length: 30 }, (_, index) => index * 10 + 10),
+      selections: 1,
+      difficultyButtons: ["easy", "medium", "hard"],
       isDragging: false,
       dialog: false,
       dialogIndex: "",
-      workout: { ...this.workoutData},
+      workout: { ...this.workoutData },
       default_exercises: null,
       searchQuery: ref(""),
     };
   },
-  mounted() {
+  created() {
     this.getDefaultExercise()
   },
   methods: {
@@ -157,12 +187,16 @@ export default {
     },
     async getDefaultExercise() {
       this.default_exercises = await WorkoutService.getDefaultExercise()
-    }, 
-    getDefaultExerciseName(training, exIndex){
-       return this.default_exercises.find(item => item._id === training.exercises[exIndex].exerciseId._id).name
-    }
+    },
+    getDefaultExerciseName(training, exIndex) {
+      return this.default_exercises.find(item => item._id === training.exercises[exIndex].exerciseId._id).name
+    },
+    setDifficulty(index) {
+      this.workout.difficulty = index
+    },
   },
 };
 </script>
 
 <style src="./WorkoutForm.scss" lang="scss" scoped />
+<style src="vue-scroll-picker/lib/style.css"></style>
