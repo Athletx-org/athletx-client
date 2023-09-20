2bf6ef79dd752b27fcd01c9c3ee12ff2495a9f3a
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-09-21 01:33:50 +0200

    chore: create analytics component

---
+++ src/pages/analytics/Analytics.vue
@@ -0,0 +1,12 @@
+<template>
+<h1> Analytics </h1>
+</template>
+<script>
+export default {
+  name: "Analytics",
+  data() {
+    return {}
+  }
+}
+</script>
+<style src="./Analytics.scss" lang="scss" scoped/>
