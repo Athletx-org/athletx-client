3c374dacb259eaa31858e23b1d3085b7d0943976
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-10-02 17:50:44 +0200

    chore: create goals pie chart

---
+++ src/components/pieChart/PieChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <apexchart type="pie" width="380" :options="chartOptions" :series="series"></apexchart>
+</template>
+<script>
+import VueApexCharts from "vue3-apexcharts";
+
+export default {
+  name: "pieChart",
+  props: {
+    series: [],
+    labels: []
+  },
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      chartOptions: {
+        labels: this.labels,
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }]
+      },
+    }
+  }
+}
+
+</script>
