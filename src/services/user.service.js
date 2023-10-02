29491dfe591e83348a854af00db523db3f8e3033
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-10-02 16:21:01 +0200

    chore: create methods to add and get improvements

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -13,6 +13,17 @@ class UserService{
         return axiosInstance.patch("/users/info/"+userId, userInfo,
             {headers: {...authHeader(),...contentType }})
     }
+
+    addImprovement(userId, improvement) {
+        return axiosInstance.post("/users/improvements/"+userId, improvement, {headers: authHeader() })
+    }
+
+    getUserImprovement(userId) {
+        return axiosInstance.get("/users/improvements/"+userId, {headers: authHeader() })
+            .then(response => {
+                return response.data
+            })
+    }
 }
 
 export default new UserService(); 
