a974324b60b7cf50102c012c0d102f6b160f028e
giacomo accursi <giacomo.accursi@gmail.com>
2023-10-03 16:01:18 +0200

    chore: make methods async

--- src/services/workout.service.js
+++ src/services/workout.service.js
@@ -3,26 +3,19 @@ import authHeader from "@/services/auth-header";
 
 class WorkoutService {
 
-    getWorkouts(userId) {
-        return axiosInstance.get("/" + userId + "/workouts", {headers: authHeader()})
-            .then(response => {
-                return response.data
-            })
+    async getWorkouts(userId) {
+        const response = await axiosInstance.get("/" + userId + "/workouts", { headers: authHeader() });
+        return response.data;
     }
 
-    getWorkout(userId, workoutId) {
-        return axiosInstance.get("/" + userId + "/workouts/" + workoutId, {headers: authHeader()})
-            .then(response => {
-                return response.data
-            })
+    async getWorkout(userId, workoutId) {
+        const response = await axiosInstance.get("/" + userId + "/workouts/" + workoutId, { headers: authHeader() });
+        return response.data;
     }
 
-    getCurrentWorkout(userId) {
-        return axiosInstance.get("/" + userId + "/workouts/info/current", {headers: authHeader()})
-            .then( response => {
-                return response.data
-            }
-        )
+    async getCurrentWorkout(userId) {
+        const response = await axiosInstance.get("/" + userId + "/workouts/info/current", { headers: authHeader() });
+        return response.data;
     }
 
     deleteWorkout(userId, workoutId) {
