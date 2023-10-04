8458f6d288b399eb3f5cf5b6818628395b3c7333
giacomo accursi <giacomo.accursi@gmail.com>
2023-10-04 15:36:04 +0200

    chore: add line chart to analytics page

--- src/components/bodyMeasuresLineChart/BodyMeasuresLineChart.vue
+++ src/components/bodyMeasuresLineChart/BodyMeasuresLineChart.vue
@@ -3,59 +3,61 @@
 </template>
 <script>
 import VueApexCharts from "vue3-apexcharts";
+import UserService from "@/services/user.service";
 
 export default {
-    name: "Analytics",
     components: {
         apexchart: VueApexCharts,
     },
     data() {
         return {
-            series: [{
-                name: 'Sales',
-                data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5]
-            },
-            {
-                name: 'Sales',
-                data: [4, 3, 10, 9, 9, 19, 12, 10, 12, 7, 19, 5, 33, 9, 17, 5, 7, 9]
-            }
-        ],
             chartOptions: {
-                title: {
-                    text: 'Body Measures',
-                    align: 'left',
-                    style: {
-                        fontSize: "16px",
-                        color: '#666'
-                    }
-                },
                 chart: {
-                    height: 350,
-                    type: 'line',
-                },
-                stroke: {
-                    width: 5,
-                    curve: 'smooth'
+                    type: "line"
                 },
                 xaxis: {
-                    type: 'datetime',
-                    categories: ['1/11/2000', '2/11/2000', '3/11/2000', '4/11/2000', '5/11/2000', '6/11/2000', '7/11/2000', '8/11/2000', '9/11/2000', '10/11/2000', '11/11/2000', '12/11/2000', '1/11/2001', '2/11/2001', '3/11/2001', '4/11/2001', '5/11/2001', '6/11/2001'],
-                    tickAmount: 10,
-                    labels: {
-                        formatter: function (value, timestamp, opts) {
-                            return opts.dateFormatter(new Date(timestamp), 'dd MMM')
-                        }
-                    }
-                },
-
-                yaxis: {
-                    min: -10,
-                    max: 40
+                    type: "datetime",
                 }
             },
+            series: [
+                {
+                    name: "Chest",
+                    data: []
+                },
+                {
+                    name: "Biceps",
+                    data: []
+                },
+                {
+                    name: "Quadriceps",
+                    data: []
+                }
+            ]
         }
+    },
+    methods: {
+        loadBodyMeasure() {
+            UserService.getAllUserImprovements(this.$store.state.auth.user._id).then((improvements) => {
+                improvements.slice(0, 10)
+                const dateTime = improvements.map(improvement => improvement.timeStamp)
+                const chest = improvements.map(improvement => improvement.chest)
+                this.series[0].data = dateTime.map((time, index) => {
+                    return [time, chest[index]]
+                })
+                const biceps = improvements.map(improvement => improvement.biceps)
+                this.series[1].data = dateTime.map((time, index) => {
+                    return [time, biceps[index]]
+                })
+                const quadriceps = improvements.map(improvement => improvement.quadriceps)
+                this.series[2].data = dateTime.map((time, index) => {
+                    return [time, quadriceps[index]]
+                })
+            })
+        },
+    },
+    created() {
+        this.loadBodyMeasure()
     }
+
 }
-
-
 </script>
